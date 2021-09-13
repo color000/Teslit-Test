5f65f7be9435a13907d33f5797bcce957318b8ee
color000 <backgroundcolorfff@gmail.com>
2021-09-13 17:31:45 +0900

    css

--- js/common.js
+++ js/common.js
@@ -3,14 +3,7 @@ $(function(){
         slidesToShow: 1,
         slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 3000,
-        arrows: false,
-      });
-      $('.news_slide').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 3000,
+        autoplaySpeed: 5000,
         arrows: false,
       });
 
@@ -25,6 +18,11 @@ $(function(){
         pagination: {
           el: ".swiper-pagination",
           clickable: true,
+        },
+        loop: true,
+        autoplay : {  // 자동 슬라이드 설정 , 비 활성화 시 false
+          delay : 7000,   // 시간 설정
+          disableOnInteraction : false,  // false로 설정하면 스와이프 후 자동 재생이 비활성화 되지 않음
         },
       });
 })
