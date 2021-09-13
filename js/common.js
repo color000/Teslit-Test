32bc87b3d0609c5d46a553ac9eb09859a1d70847
color000 <backgroundcolorfff@gmail.com>
2021-09-13 17:36:52 +0900

    css

--- js/common.js
+++ js/common.js
@@ -19,10 +19,9 @@ $(function(){
           el: ".swiper-pagination",
           clickable: true,
         },
-        loop: true,
-        autoplay : {  // 자동 슬라이드 설정 , 비 활성화 시 false
-          delay : 7000,   // 시간 설정
-          disableOnInteraction : false,  // false로 설정하면 스와이프 후 자동 재생이 비활성화 되지 않음
+          autoplay: {
+          delay: 2500,
+          disableOnInteraction: false,
         },
       });
 })
