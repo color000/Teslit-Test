c32d167dfba60dffa196c589585b9304002f8cfc
color000 <backgroundcolorfff@gmail.com>
2021-09-14 11:28:56 +0900

    header 색상 변경

--- js/common.js
+++ js/common.js
@@ -1,5 +1,7 @@
 $(function(){
-    $('.slick_wrap').slick({
+      
+      // 메인 배너
+      $('.slick_wrap').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
         autoplay: true,
@@ -7,11 +9,13 @@ $(function(){
         arrows: false,
       });
 
+      // 남성, 여성 클릭시 on, off 
       $('.gender a').click(function(){
         $(this).removeClass('off');
         $(this).siblings().addClass('off');
       })
 
+      // News
       var swiper = new Swiper(".mySwiper", {
         slidesPerView: "auto",
         spaceBetween: 25,
@@ -28,18 +32,21 @@ $(function(){
         $('body').addClass('modal_open');
       });
 
+      // 사이드 메뉴 X버튼 클릭시 팝업 닫힘
       $('.popup_close').click(function () {
         $('.dim').hide();
         $('.home_sidemenu').removeClass('modal_open');
         $('body').removeClass('modal_open');
       });
 
+      // 검은 배경 클릭시 팝업 닫힘
       $('.dim').click(function(){
       	$(".dim").hide();
         $('.home_sidemenu').removeClass('modal_open');
         $('body').removeClass('modal_open');
       });
 
+      // 스크롤 되면 header 색상 들어가게
       $(window).scroll(function() {
         var scroll = $(window).scrollTop();
         if (scroll >= 50) {
