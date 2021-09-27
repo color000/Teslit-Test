95c8ac598a02ab18e9562332e5cb893896da5fbc
color000 <backgroundcolorfff@gmail.com>
2021-09-27 14:58:42 +0900

    comment

--- js/common.js
+++ js/common.js
@@ -73,6 +73,11 @@ $(function(){
         $(this).siblings('.edit_popup').show();
         return false; //중요
       });
+
+      $(".pro_header .write").click(function () {
+        $(this).siblings('.edit_popup').show();
+        return false; //중요
+      });
    
       // 외부영역 클릭 시 팝업 닫기
       $(document).mouseup(function (e){
